94b84e3275271e18c7dddcb5e9fd0049d7e9da76
divinoe <emidio.divino@tasgroup.eu>
2025-09-24 12:37:49 +0200

    aggiunti componenti figli articoli

--- src/app/modules/articoli/pages/grid-articoli/grid-articoli.component.ts
+++ src/app/modules/articoli/pages/grid-articoli/grid-articoli.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ArticoliService } from 'src/app/core/services/articoli.service';
+import { IArticoli } from 'src/app/shared/models/Articoli';
 
 @Component({
   selector: 'app-grid-articoli',
   templateUrl: './grid-articoli.component.html',
   styleUrls: ['./grid-articoli.component.css']
 })
-export class GridArticoliComponent {
+export class GridArticoliComponent implements OnInit {
+
+  articoli$: IArticoli[] = [];
+
+  constructor(private articoliService: ArticoliService) {
+
+    this.articoli$ = this.articoliService.getArticoli();
+    console.log(this.articoli$);
+
+  }
+
+  handleEdit = (codart: string) => {
+    console.log("Cliccato tasto modifica del codice " + codart);
+  }
+
+  handleDelete = (codart: string) => {
+    console.log("Cliccato tasto elimina del codice " + codart);
+
+    this.articoli$.splice(this.articoli$.findIndex(x => x.codart === codart), 1)
+    console.log(this.articoli$);
+  }
+
+  ngOnInit(): void { }
 
 }
