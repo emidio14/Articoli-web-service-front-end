dec31f39703f9e96d7b7a0a808c5dc8db157de82
divinoe <emidio.divino@tasgroup.eu>
2025-09-24 09:22:58 +0200

    nuove funzionalità

--- src/app/modules/articoli/articoli.module.ts
+++ src/app/modules/articoli/articoli.module.ts
@@ -2,10 +2,12 @@ import { ArticoliComponent } from './pages/articoli/articoli.component';
 import { CommonModule } from '@angular/common';
 import { CoreModule } from 'src/app/core/core.module';
 import { NgModule } from '@angular/core';
+import { GridArticoliComponent } from './pages/grid-articoli/grid-articoli.component';
 
 @NgModule({
   declarations: [
     ArticoliComponent,
+    GridArticoliComponent,
   ],
   imports: [
     CommonModule,
