3203d03e4ebb19a7b714a71b97ac1e35cbe51433
divinoe <emidio.divino@tasgroup.eu>
2025-09-24 09:45:18 +0200

    sotto componenti creati

--- src/app/modules/articoli/articoli.module.ts
+++ src/app/modules/articoli/articoli.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { CoreModule } from 'src/app/core/core.module';
 import { NgModule } from '@angular/core';
 import { GridArticoliComponent } from './pages/grid-articoli/grid-articoli.component';
+import { ArticoliCardComponent } from './components/articoli-card/articoli-card.component';
 
 @NgModule({
   declarations: [
     ArticoliComponent,
     GridArticoliComponent,
+    ArticoliCardComponent,
   ],
   imports: [
     CommonModule,
